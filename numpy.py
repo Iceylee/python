00ecafb8c43869e7a3885bdc634df43d5bfce14f
Icey Lee <iceyleemj@gmail.com>
2018-01-29 11:28:00 +0800

    numpy

---
+++ numpy.py
@@ -0,0 +1,143 @@
+# height is available as a regular list
+
+# Import numpy
+import numpy as np
+
+# Create a numpy array from height: np_height
+np_height = np.array(height)
+
+# Print out np_height
+print(np_height)
+
+# Convert np_height to m: np_height_m
+np_height_m = np_height * 0.0254
+print (np_height_m)
+
+##
+# Calculate the BMI: bmi
+np_height_m = np.array(height) * 0.0254
+np_weight_kg = np.array(weight) * 0.453592
+bmi = np_weight_kg / np_height_m ** 2
+
+# Create the light array
+light = bmi < 21
+print(light)
+print(bmi[light])
+
+# Print out sub-array of np_height: index 100 up to and including index 110
+print(np_height[100:111])
+
+#2.二维array
+# Create baseball, a list of lists
+baseball = [[180, 78.4],
+            [215, 102.7],
+            [210, 98.5],
+            [188, 75.2]]
+
+# Import numpy
+import numpy as np
+
+# Create a 2D numpy array from baseball: np_baseball
+np_baseball = np.array(baseball)
+
+# Print out the type of np_baseball
+print(type(np_baseball))
+
+# Print out the shape of np_baseball
+print(np_baseball.shape)
+
+# Create np_baseball (2 cols)
+np_baseball = np.array(baseball)
+
+# Print out the 50th row of np_baseball
+print(np_baseball[49,:])
+
+# Select the entire second column of np_baseball: np_weight
+np_weight = np_baseball[:,1]
+
+
+# Print out height of 124th player
+print(np_baseball[123,0])
+
+# Print out addition of np_baseball and updated
+print(np_baseball + updated)
+
+# Create numpy array: conversion
+conversion = np.array([0.0254,0.453592,1])
+
+# Print out product of np_baseball and conversion
+print(np_baseball * conversion)
+
+##3.basic statistics
+# Create np_height from np_baseball
+np_height = np.array(np_baseball[:,0])
+
+# Print mean height (first column)
+avg = np.mean(np_baseball[:,0])
+print("Average: " + str(avg))
+
+# Print median height. Replace 'None'
+med = np.median(np_baseball[:,0])
+print("Median: " + str(med))
+
+# Print out the standard deviation on height. Replace 'None'
+stddev = np.std(np_baseball[:,0])
+print("Standard Deviation: " + str(stddev))
+
+# Print out correlation between first and second column. Replace 'None'
+corr = np.corrcoef(np_baseball[:,0])
+print("Correlation: " + str(corr))
+
+# Heights of the goalkeepers: gk_heights
+gk_heights = np_heights[np_positions == 'GK']
+
+# Heights of the other players: other_heights
+other_heights = np_heights[np_positions != 'GK']
+
+
+##1.dot plot-line plot
+# Print the last item from year and pop
+print(year[-1])
+print(pop[-1])
+
+# Import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+
+# Make a line plot: year on the x-axis, pop on the y-axis
+plt.plot(year,pop)
+
+# Display the plot with plt.show()
+plt.show()
+
+# Change the line plot below to a scatter plot
+plt.scatter(gdp_cap, life_exp)
+
+# Put the x-axis on a logarithmic scale
+plt.xscale('log')
+
+# Show plot
+plt.show()
+
+#2.histogram
+# Create histogram of life_exp data,自动设为10
+plt.hist(life_exp)
+
+# Display histogram
+plt.show()
+
+# Build histogram with 5 bins
+plt.hist(life_exp,bins =5)
+
+# Show and clean up plot
+plt.show()
+plt.clf()
+
+# Build histogram with 20 bins
+plt.hist(life_exp,bins = 20)
+
+# Show and clean up again
+plt.show()
+plt.clf()
+
+#3.customization
+
